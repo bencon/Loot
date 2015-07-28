3bc14e65d80012340053fd16be2dc09930ec9be7
Ben Llewelyn <bencon@udel.edu>
2015-07-27 21:53:32 -0400

    Extract now pulls the title/description that will soon be used to filter for desired items

--- extract2.py
+++ extract2.py
@@ -28,54 +28,61 @@ def parseCraigslistRSS(hyperlink):
     print "start"
     #for link in soup.find_all('link'):
     for item in soup.find_all({'item' : True}):
-            linkDescriptionArr = []
-            info = ""
-            for link in item.find('link'):
-                linkDescriptionArr.append(link.string)
-            for title in item.find("title"):
-                info += str(title.string)
-            for description in item.find("description"):
-                info += str(description.string)
-            linkDescriptionArr.append(info)
-            results.append(linkDescriptionArr)
+        linkDescriptionArr = []
+        info = ""
+        for link in item.find('link'):
+            linkDescriptionArr.append(link.string)
+        for title in item.find("title"):
+            info += str(title.string)
+        for description in item.find("description"):
+            info += str(description.string)
+        linkDescriptionArr.append(info)
+        results.append(linkDescriptionArr)
 
-            #print item
+        #print item
     for arr in results:
         print arr
     maps = []
 
     coords=[]
     for item in results:
-            r = requests.get(item[0])
-            #print r.status_code
-            if (r.status_code == 200):
-                    #print "Yes"
-                    newSoup = BeautifulSoup(r.text)
-                    for link in newSoup.find_all('a'):
-                            url = link.get('href')
-                            if (url and "maps.google.com" in link.get('href')):
-                                    #print (link.get('href'))
-                                    #search = re.search(
-                                    for a in (link.get('href')).split('/'):
-                                            b = a.replace('@','')
-                                            if b:
-                                                    if ((b[0]).isdigit()):
-                                                            c = b.split(',')
-                                                            print c
-                                                            coords.append([c, item[1]])
+        print "item in results"
+        r = requests.get(item[0]) #item[0] is a link from the rss feed
+        #print r.status_code
+        if (r.status_code == 200):
+            #print "Yes"
+            newSoup = BeautifulSoup(r.text)
+            for link in newSoup.find_all('a'):
+                url = link.get('href')
+                if (url and "maps.google.com" in link.get('href')):
+                    #print (link.get('href'))
+                    #search = re.search(
+                    for a in (link.get('href')).split('/'):
+                        b = a.replace('@','')
+                        if b:
+                            if ((b[0]).isdigit()):
+                                c = b.split(',')
+                                print c
+                                coords.append([c, item[1]])
+                        else:
+                            print "failure"
+                else:
+                    #print "unsuccessful finding google maps in the url"
+        else :
+            print "HTML error code"
 
-    print coords
+    #print coords
     ####  Generate XML document wtih coordinate data
     root = ET.Element("root")
 
     for arr in coords:
-            field = ET.SubElement(root,"loc")
-            x = ET.SubElement(field,'x')
-            y = ET.SubElement(field,'y')
-            description = ET.SubElement(field,'desc')
-            x.text = (str)(arr[0][0])
-            y.text = (str)(arr[0][1])
-            description.text = arr[1]
+        field = ET.SubElement(root,"loc")
+        x = ET.SubElement(field,'x')
+        y = ET.SubElement(field,'y')
+        description = ET.SubElement(field,'desc')
+        x.text = (str)(arr[0][0])
+        y.text = (str)(arr[0][1])
+        description.text = arr[1]
 
     tree = ET.ElementTree(root)
     tree.write("coords.xml")
