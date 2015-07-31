09ef5f06d2454ea4f95819483fc68148e9b3717c
Ben Llewelyn <bencon@udel.edu>
2015-07-30 22:30:16 -0400

    Now handles alternate style google map hyperlink for more results

--- extract2.py
+++ extract2.py
@@ -20,12 +20,10 @@ def parseCraigslistRSS(hyperlink):
     print r.headers['content-type']
 
     soup = BeautifulSoup(r.text, 'xml')
-    print (soup.prettify())
+    #print (soup.prettify())
 
-    print soup.link
-    #print soup
+    #print soup.link
     results = []
-    print "start"
     #for link in soup.find_all('link'):
     for item in soup.find_all({'item' : True}):
         linkDescriptionArr = []
@@ -36,42 +34,35 @@ def parseCraigslistRSS(hyperlink):
             info += str(title.string)
         for description in item.find("description"):
             info += str(description.string)
+        info = info.replace('\n', ' ').replace('\r', '')
         linkDescriptionArr.append(info)
         results.append(linkDescriptionArr)
 
-        #print item
-    for arr in results:
-        print arr
-    maps = []
-
     coords=[]
+    addresses=[]
     for item in results:
-        print "item in results"
+        print "\r"
+        print item[1]
         r = requests.get(item[0]) #item[0] is a link from the rss feed
-        #print r.status_code
         if (r.status_code == 200):
-            #print "Yes"
             newSoup = BeautifulSoup(r.text)
             for link in newSoup.find_all('a'):
                 url = link.get('href')
                 if (url and "maps.google.com" in link.get('href')):
-                    #print (link.get('href'))
-                    #search = re.search(
-                    for a in (link.get('href')).split('/'):
-                        b = a.replace('@','')
-                        if b:
-                            if ((b[0]).isdigit()):
-                                c = b.split(',')
-                                print c
-                                coords.append([c, item[1]])
-                        else:
-                            print "failure"
-                else:
-                    #print "unsuccessful finding google maps in the url"
-        else :
-            print "HTML error code"
+                    print (link.get('href'))
+                    splitString = link.get('href').split('@')
+                    attempt2 = link.get('href').split('?q=loc')
+                    if (len(splitString) > 1 and splitString[1]):
+                        coordinates = splitString[1]
+                        print coordinates + " <----- SUCCESS"
+                        # Put coordinates in array form and add coordinates/ description pair to array
+                        coords.append([coordinates.split(','), item[1]])
+                    elif (len(attempt2) > 1):
+                        address = attempt2[1]
+                        print address + "<------ SUCCESS V2"
+                        addresses.append([address, item[1]])
 
-    #print coords
+
     ####  Generate XML document wtih coordinate data
     root = ET.Element("root")
 
@@ -84,10 +75,20 @@ def parseCraigslistRSS(hyperlink):
         y.text = (str)(arr[0][1])
         description.text = arr[1]
 
+    for arr in addresses:
+        field = ET.SubElement(root,"AddressString")
+        addr = ET.SubElement(field,'addr')
+        description = ET.SubElement(field,'desc')
+        addr.text = (str)(arr[0])
+        description.text = arr[1]
+
+
     tree = ET.ElementTree(root)
     tree.write("coords.xml")
 
 
 if __name__ == "__main__":
-    parseCraigslistRSS('https://philadelphia.craigslist.org/zip/index.rss')
+    parseCraigslistRSS('https://philadelphia.craigslist.org/zip/index.rss');  #craigslist free
+    #parseCraigslistRSS('https://philadelphia.craigslist.org/search/sss?format=rss');
+    #parseCraigslistRSS('https://philadelphia.craigslist.org/search/sss?query=glass%20carboy&format=rss');  #glass carboy search
 
